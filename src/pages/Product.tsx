4a964ca07075732113e4116954c8393c46e0b7ab
Ricardo <rmvnew@gmail.com>
2022-12-03 22:09:16 -0400

    added page to list products

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,14 +1,145 @@
+import { Pagination } from "@mui/material"
+import { useEffect, useState } from "react"
+import { NavLink } from "react-router-dom"
+import { api } from "../hooks/useApi"
 
 
 
 const Product = () => {
+    function setResponse(res: any) {
 
-    return (
-        <>
-            <h1>Produtos</h1>
-        </>
-    )
-
+        // console.log('Res: ',res);
+    
+        setProducts(res.data.items)
+        setPages(res.data.meta.totalPages)
+    
+      }
+    
+      const handleChange = (e: any, currentPage: any) => {
+        setPage(currentPage)
+      }
+    
+    
+      const [products, setProducts] = useState<any[]>([])
+      const [pages, setPages] = useState(0)
+      const [page, setPage] = useState(1)
+      const [search, setSearch] = useState("")
+    
+      const getProduct = async (page: number = 1) => {
+    
+        await api.get(`/product?page=${page}&limit=8&sort=DESC&orderBy=ID`).then((response) => {
+         
+          setResponse(response)
+        });
+      };
+      
+      
+      const getProductByName = async (page: number = 1,name:string = "") => {
+    
+        await api.get(`/product?page=${page}&limit=8&sort=DESC&orderBy=ID&search=${name}`).then((response) => {
+          setResponse(response)
+        });
+      };
+    
+    
+    
+      const deleteProduct = async (product_id: number = 0) => {
+    
+        
+    
+        await api.delete(`/product/${product_id}`)
+          .then(response => {
+            getProduct();
+          }).catch(error => {
+            console.log(error);
+          })
+      }
+    
+    
+      useEffect(()=>{
+        getProductByName(page,search)
+      },[search])
+    
+      useEffect(() => {
+        getProduct();
+      }, []);
+    
+    
+    
+      useEffect(() => {
+        getProduct(page);
+      }, [page])
+    
+    
+      return (
+        <div className="main">
+          <h1>Gerenciamento de usuários</h1>
+    
+    
+    
+    
+          <section className="links">
+            <NavLink to={"/user/form"} className="btn btn-primary newUser">Novo Produto</NavLink>
+          </section>
+    
+       
+    
+          <input
+            className="form-control form-control-lg search"
+            type="text"
+            placeholder="Busca de produtos"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+    
+          <table className="my-table table table-striped">
+            <thead>
+              <tr>
+                <td>id</td>
+                <td>Nome</td>
+                <td>Código de barras</td>
+                <td>Quantidade</td>
+                <td>preço</td>
+                <td>Status</td>
+                <td>Opções</td>
+              </tr>
+            </thead>
+            <tbody>
+              {products &&
+                products.map((product) => (
+                  <tr key={product.product_id}>
+                    <td>{product.product_id}</td>
+                    <td>{product.product_name}</td>
+                    <td>{product.product_barcode}</td>
+                    <td>{product.product_quantity}</td>
+                    <td>{product.product_price.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</td>
+                    <td>{product.is_active === true ? "Ativo" : "Inativo"}</td>
+                    <td>
+    
+                      <div className="d-flex justify-content-around base-options">
+    
+                        <NavLink to={"/user/form"} state={{
+                          data: {
+                            product_id: product.user_id,
+                            
+                          }
+                        }} className="btn btn-warning">Editar</NavLink>
+    
+                        <button className="btn btn-danger btn-delete"
+                          onClick={() => deleteProduct(product.product_id)}
+                        >Deletar</button>
+    
+                      </div>
+    
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+    
+          <Pagination count={pages} color="primary" onChange={handleChange}></Pagination>
+        </div>
+      );
 }
 
 export default Product
