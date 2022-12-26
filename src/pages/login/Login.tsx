78d4f8d2d1e99a0528e851aec6ac8c226446e306
Ricardo <rmvnew@gmail.com>
2022-12-25 21:34:19 -0400

    refactor: change alert to toastfy

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -3,10 +3,8 @@ import { AuthContext } from '../../contexts/auth/AuthContext';
 import '../../common/css/bootstrap.min.css'
 import { ModalLogin } from '../../components/Modal.login';
 import { RecoverForm } from '../../components/recover-password/recover-password';
-import { AlertTypes } from '../../enums/enums';
-import { AlertTypesInterface } from '../../interfaces/AlertTypesInterface';
-import AlertMessage from '../../components/AlertMessage';
 import { LoginCard, LoginInputs, LoginTitle } from './LoginStyled';
+import { toast } from 'react-toastify';
 
 
 
@@ -16,25 +14,30 @@ import { LoginCard, LoginInputs, LoginTitle } from './LoginStyled';
 const Login = () => {
 
 
-    function showAlert(type: AlertTypes, message: string, time: number) {
-
-
-
-        const props: AlertTypesInterface = {
-            message: message,
-            aletTypes: type,
-            time: time
+    const msgToast = (msg: string, timer: number, type: boolean) => {
+        if (type) {
+            toast.success(msg, {
+                position: "top-right",
+                autoClose: timer,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "colored",
+            })
+        } else {
+            toast.error(msg, {
+                position: "bottom-center",
+                autoClose: timer,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "colored",
+            })
         }
-
-        setAlertProps(props)
-
-        setOpen(true)
-
-        setTimeout(() => {
-            setOpen(false)
-
-        }, time)
-
     }
 
 
@@ -43,8 +46,7 @@ const Login = () => {
 
     const [email, setEmail] = useState('rmvnew@gmail.com')
     const [password, setPassword] = useState('12345')
-    const [open, setOpen] = useState(false);
-    const [alertProps, setAlertProps] = useState({})
+   
 
     const handleLogin = async () => {
 
@@ -56,7 +58,7 @@ const Login = () => {
 
             if (isLogged.status) {
                 console.log('logou');
-                showAlert(AlertTypes.SUCCESS, "Bem vindo!!", 1000)
+                msgToast('Bem vindo!!', 2000,true)
                 setTimeout(() => {
                     window.location.href = '/'
                 }, 1000)
@@ -64,9 +66,9 @@ const Login = () => {
                 console.log('não logou');
                 console.log(isLogged.code);
                 if (isLogged.code === 401) {
-                    showAlert(AlertTypes.ERROR, "Senha inválida", 4000)
+                    msgToast('Senha inválida', 4000,false)
                 } else if (isLogged.code === 404) {
-                    showAlert(AlertTypes.ERROR, "Usuário inválido", 4000)
+                    msgToast('Usuário inválido', 4000,false)
                 }
             }
         }
@@ -85,7 +87,7 @@ const Login = () => {
                     <LoginTitle>
                         <h2>Login</h2>
                     </LoginTitle>
-                    
+
                     <input
                         className='form-control form-control-lg'
                         type="text"
@@ -106,7 +108,7 @@ const Login = () => {
                     <button className='btn btn-primary' onClick={handleLogin}>Logar</button>
                 </LoginInputs>
             </LoginCard>
-            {open && <AlertMessage props={alertProps} />}
+            
         </>
     )
 
